cbefaa7cdeaae90ce8bebf2f47589c40734a6677
Procurio-AG <1108.anantgupta@gmail.com>
2025-04-24 04:08:33 +0530

    E: Complete textnode and initial main with unittest

--- src/main.py
+++ src/main.py
@@ -1,4 +1,22 @@
 from textnode import TextNode,TextType
+from htmlnode import *
+
+def text_node_to_html_node(textnode):
+    match(textnode.text_type):
+        case TextType.TEXT:
+            return LeafNode(None,value=textnode.text)
+        case TextType.BOLD:
+            return LeafNode('b',value=textnode.text)        
+        case TextType.ITALIC:
+            return LeafNode('i',value=textnode.text)
+        case TextType.CODE:
+            return LeafNode('code',value=textnode.text)
+        case TextType.LINK:
+            return LeafNode('a',value=textnode.text,props={"href":textnode.url})
+        case TextType.IMAGE:
+            return LeafNode('img',None,props={"src":textnode.url, "alt":textnode.text})
+        case _:
+            raise Exception("Invalid texttype")
 
 def main():
     t1 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -24,17 +24,3 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-        
-
-'''if TextType.self.text_type == "normal":
-            return f"TextNode(**{self.text}**,{self.text_type}**,{self.url})"
-        elif TextType.self.text_type == "bold":
-            return f"TextNode(**{self.text}**,{self.text_type},{self.url})"
-        elif TextType.self.text_type == "italic":
-            return f"TextNode(_{self.text}_,{self.text_type},{self.url})"
-        elif TextType.self.text_type == "code":
-            return f"TextNode(`{self.text}`,{self.text_type},{self.url})"
-        elif TextType.self.text_type == "link":
-            return f"TextNode([{self.text}],({self.url}))"
-        elif TextType.self.text_type == "url":
-            return f"TextNode(![{self.text}],({self.url}))"'''
